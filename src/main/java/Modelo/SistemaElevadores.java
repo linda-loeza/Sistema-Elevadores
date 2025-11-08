a5a42925efe1d756287e7f6767cdbdea7629fe70
Natalia Loeza <A21203463@alumnos.uady.mx>
2025-11-07 20:26:58 -0600

    Se agrega el proyecto

---
+++ src/main/java/Modelo/SistemaElevadores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Modelo;
+
+/**
+ *
+ * @author natalia-loeza
+ */
+public class SistemaElevadores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
